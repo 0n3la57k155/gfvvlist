45e42ca79df6015ed7aee2cbd37464071b8631a4
ky0ncheng <kawaiiushio@gmail.com>
2016-03-10 12:51:20 +0800

    update

--- go.sh
+++
@@ -1,4 +0,0 @@
-python make.py -p "SOCKS5 127.0.0.1:1080" -b ./blackList -o ./src/socks5proxyb.pac -m
-python make.py -p "SOCKS5 127.0.0.1:1080" -w ./whiteList -o ./src/socks5proxyw.pac -m
-python make.py -p "PROXY 10.0.0.140:7777" -b ./blacklist -o ./src/httpproxyb.pac -m
-python make.py -p "PROXY 10.0.0.140:7777" -w ./whitelist -o ./src/httpproxyw.pac -m
